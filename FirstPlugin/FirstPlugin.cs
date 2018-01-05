7a246f49ab4b979c3624297f995d7bab7950452d
Ryuzaki <lod.skot@gmail.com>
2018-01-05 08:36:56 +0400

    Upload

---
+++ FirstPlugin/FirstPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ensage;
+using Ensage.SDK.Service;
+using Ensage.SDK.Service.Metadata;
+
+namespace FirstPlugin
+{
+    public class FirstPlugin : Plugin
+    {
+        static void Main(string[] args)
+        {
+            Game.OnStart += GameOnOnStart;
+            Game.OnIngameUpdate += GameOnOnIngameUpdate;
+            Game.OnMessage += GameOnOnMessage;
+        }
+
+        private static void GameOnOnMessage(MessageEventArgs args)
+        {
+            Game.ExecuteCommand("say GameOnOnMessage");
+        }
+
+        private static void GameOnOnIngameUpdate(EventArgs args)
+        {
+            Game.ExecuteCommand("say GameOnOnIngameUpdate");
+        }
+
+        private static void GameOnOnStart(EventArgs args)
+        {
+            Game.ExecuteCommand("say GameOnOnStart");
+        }
+    }
+}
