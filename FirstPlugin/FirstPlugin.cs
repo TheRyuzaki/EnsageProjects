74308d33cbded7b990fa32ff6eb6cdc3645e4ab5
Ryuzaki <lod.skot@gmail.com>
2018-01-05 09:17:49 +0400

    321123

--- FirstPlugin/FirstPlugin.cs
+++ FirstPlugin/FirstPlugin.cs
@@ -16,8 +16,6 @@ namespace FirstPlugin
             Game.OnMessage += GameOnOnMessage;
             Game.OnFireEvent += GameOnOnFireEvent;
             Game.OnGCMessageReceive += GameOnOnGcMessageReceive;
-            Game.PrintMessage("Hello world");
-            
         }
 
         private static void GameOnOnGcMessageReceive(GCMessageEventArgs args)
@@ -33,29 +31,24 @@ namespace FirstPlugin
                 case "dota_game_state_change":
                     Game.ExecuteCommand("say Status: " + Game.GameState);
                     break;
-                case "dota_portrait_unit_stats_changed":
-                    Game.ExecuteCommand("say Игроки в сессии:");
-                    Game.ExecuteCommand("say [0] => [" + Players.All[0].PlayerSteamId + " / " + Players.All[0].Name + "]");
-                    break;
             }
         }
 
         private static void GameOnOnMessage(MessageEventArgs args)
         {
             Game.ExecuteCommand("say GameOnOnMessage");
-            Game.PrintMessage("Hello world1");
         }
 
         private static void GameOnOnIngameUpdate(EventArgs args)
         {
             //Game.ExecuteCommand("say GameOnOnIngameUpdate");
-            Game.PrintMessage("Hello world2");
         }
 
         private static void GameOnOnStart(EventArgs args)
         {
             Game.ExecuteCommand("say GameOnOnStart");
-            Game.PrintMessage("Hello world3");
+            Game.ExecuteCommand("say Игроки в сессии:");
+            Game.ExecuteCommand("say [0] => [" + Players.All[0].PlayerSteamId + " / " + Players.All[0].Name + "]");
         }
     }
 }
